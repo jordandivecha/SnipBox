82d060bfbe3792aa208ff3a628572e2d767a9081
Jordan Chadsey <jordan@identipop.com>
2017-11-13 23:12:22 -0800

    removed preload

--- assets/design/style.js
+++ assets/design/style.js
@@ -1,30 +1,10 @@
 
-function preloader() {
-	if (document.getElementById) {
-		document.getElementById("preload-01").style.background = "url(http://domain.tld/image-01.png) no-repeat -9999px -9999px";
-		document.getElementById("preload-02").style.background = "url(http://domain.tld/image-02.png) no-repeat -9999px -9999px";
-		document.getElementById("preload-03").style.background = "url(http://domain.tld/image-03.png) no-repeat -9999px -9999px";
-	}
-}
-function addLoadEvent(func) {
-	var oldonload = window.onload;
-	if (typeof window.onload != 'function') {
-		window.onload = func;
-	} else {
-		window.onload = function() {
-			if (oldonload) {
-				oldonload();
-			}
-			func();
-		}
-	}
-}
-addLoadEvent(preloader);
+
 
 $(document).ready(function(){
 //    $(".tabs").swipeable(true);
 
-
+Materialize.toast('I am a toast!', 3000, 'rounded') // 'rounded' is the class I'm applying to the toast
 
 $('.collapsible').collapsible();
  // Open
